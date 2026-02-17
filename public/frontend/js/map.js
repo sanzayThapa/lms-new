4f02238977130177052cee062d4227aaf5e80923
sanzayThapa <sanjaythapa9808@gmail.com>
2026-02-17 15:11:38 +0545

    added laravel and setup react for frontend

--- public/frontend/js/map.js
+++ public/frontend/js/map.js
@@ -3,7 +3,7 @@ var mymap = L.map('map', {
     zoom: 13
 });
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk', {
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
         '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
